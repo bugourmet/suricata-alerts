94eb700bd67f88532ef020ed94b2fd8508b01c90
runtimeterrorist <67608248+runtimeterrorist@users.noreply.github.com>
2024-05-18 21:43:25 +0200

    enable telegram message formatting

--- monitor.py
+++ monitor.py
@@ -1,10 +1,9 @@
+import asyncio
 import os
 import re
-import asyncio
-from telegram import Bot
 from datetime import datetime
 from dotenv import load_dotenv
-
+from telegram import Bot
 load_dotenv()
 
 bot_token = os.getenv("BOT_TOKEN")
@@ -15,7 +14,7 @@ priorities = os.getenv("PRIORITY", "").split('|')
 async def send_telegram_message(user_id, message):
     """Sends a Telegram message."""
     bot = Bot(token=bot_token)
-    await bot.send_message(chat_id=user_id, text=message)
+    await bot.send_message(chat_id=user_id, text=message,parse_mode='Markdown')
 
 def parse_log_timestamp(log_line):
     timestamp_match = re.search(r'\d{2}/\d{2}/\d{4}-\d{2}:\d{2}:\d{2}\.\d{6}', log_line)
@@ -42,7 +41,7 @@ async def check_log_file(filename, start_time):
                     if priority_value and str(priority_value) in priorities:
                         for user_id in allowed_user_ids:
                             message = (
-                                            f"**HIGH PRIORITY EVENT (Priority {priority_value}) DETECTED:**\n"
+                                            f"*HIGH PRIORITY EVENT (Priority {priority_value}) DETECTED:*\n"
                                             f"```\n{line.strip()}\n```"
                                         )
                             await send_telegram_message(user_id,message)
@@ -53,8 +52,8 @@ async def check_log_file(filename, start_time):
                         if attack_id in attacks:
                             for user_id in allowed_user_ids:
                                 message = (
-                                    f"**NEW ATTACK DETECTED:**\n"
-                                    f"**Attack ID:** `{attack_id}`\n"
+                                    f"*NEW ATTACK DETECTED:*\n"
+                                    f"*Attack ID:* `{attack_id}`\n"
                                     f"```\n{line.strip()}\n```"
                                 )
                                 await send_telegram_message(user_id,message)
